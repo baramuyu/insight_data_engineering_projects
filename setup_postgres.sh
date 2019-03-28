3d248561972572f1af32b9ab20466a4f0337a845
737 <baramuyu@gmail.com>
2019-03-27 17:03:59 -0700

    accesssed data on s3, pick up columns, convert to timestamp type

---
+++ setup_postgres.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+
+sudo sh -c "echo 'deb http://apt.postgresql.org/pub/repos/apt/ `lsb_release -c -s`-pgdg main' >> /etc/apt/sources.list.d/pgdg.list"
+wget --quiet -O - https://www.postgresql.org/media/keys/ACCC4CF8.asc | sudo apt-key add -
+sudo apt-get update
+
+
+sudo apt install timescaledb-postgresql-11
+
+sudo timescaledb-tune
+
+sudo service postgresql restart
+
+# missing postGIS
