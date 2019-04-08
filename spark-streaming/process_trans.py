c21af6817007290d113634f52c48383cbdd41ab2
Ubuntu <ubuntu@ip-10-0-0-11.us-west-2.compute.internal>
2019-04-08 23:25:25 +0000

    <feat> keep state on db

--- spark-streaming/process_trans.py
+++ spark-streaming/process_trans.py
@@ -7,6 +7,7 @@ from pyspark.sql import (functions as F,
                         )
 import os
 import postgres
+from pytz import timezone
 from datetime import datetime
 
 
@@ -48,16 +49,28 @@ class ProcessTrans(object):
         df = df.select('station_id','transaction_starttime', 'transaction_endtime','paid_duration','amount_usd')
         return df
     
-    def get_lastest_three_minutes(self, trans_df):
+    def load_and_save_active_transactions(self, trans_df):
+        datetime_now = datetime.now(timezone('America/Los_Angeles')).replace(tzinfo=None)
+        table = "active_transactions"
+        
+        # load
+        self.pgres_connector.set_spark(self.spark)
+        old_trans_df = self.pgres_connector.read(table)
+        
+        # merge and filter out old transactions
+        trans_df = trans_df.union(old_trans_df)
+        trans_df = trans_df.filter(trans_df.transaction_endtime > datetime_now)
+        
+        # save
+        mode = "overwrite"
+        self.pgres_connector.write(trans_df, table, mode)
+        return trans_df
+    
+    def get_lastest_minutes(self, trans_df):
         trans_df.createOrReplaceTempView("occupancy_streaming")
         # get latest timestamp within the batch
         min_df = self.spark.sql("select max(transaction_starttime) as latest_timestamp from occupancy_streaming")
-        # get latest minute hh:10:00, one min before; hh:09:00, two mins before; hh:08:00
-        min_df = min_df.withColumn("latest_timestamp", F.date_trunc('minute', min_df.latest_timestamp)) 
-        min_df = min_df.withColumn("second_timestamp", (F.unix_timestamp(min_df.latest_timestamp) - 60).cast("timestamp")) \
-            .withColumn("third_timestamp", (F.unix_timestamp(min_df.latest_timestamp) - 120) \
-            .cast("timestamp")) \
-            .collect()
+        min_df = min_df.withColumn("latest_timestamp", F.date_trunc('minute', min_df.latest_timestamp)).collect()
         return min_df
    
     def get_dimension_table(self, min_df):
@@ -68,19 +81,14 @@ class ProcessTrans(object):
     
     def create_occupancy_df(self, min_df, dim_df):
         latest_min = min_df[0][0]
-        second_latest_min = min_df[0][1]
-        third_latest_min = min_df[0][2]
         ocp_df = dim_df.withColumn("timestamp", F.lit(latest_min))
-        ocp_df = ocp_df.union(dim_df.withColumn("timestamp", F.lit(second_latest_min)))
-        ocp_df = ocp_df.union(dim_df.withColumn("timestamp", F.lit(third_latest_min)))
-
         ocp_df.printSchema()
         return ocp_df
     
     def calc_occupancy_per_minute(self, ocp_df):
         ocp_df.createOrReplaceTempView("occupancy_perminute")
 
-        sql = ("SELECT p.station_id, p.timestamp, count(*) AS occupied_spots "
+        sql = ("SELECT p.station_id, p.timestamp, CAST(count(*) as int) AS occupied_spots "
                "FROM occupancy_perminute p LEFT OUTER JOIN occupancy_streaming s "
                "ON p.station_id = s.station_id "
                "AND p.timestamp BETWEEN s.transaction_starttime AND s.transaction_endtime "
@@ -99,7 +107,8 @@ class ProcessTrans(object):
             return
 
         trans_df = self.manipulate_trans(trans_df)
-        min_df = self.get_lastest_three_minutes(trans_df)
+        trans_df = self.load_and_save_active_transactions(trans_df)
+        min_df = self.get_lastest_minutes(trans_df)
         dim_df = self.get_dimension_table(min_df)
         ocp_df = self.create_occupancy_df(min_df, dim_df)
         occupancy_per_min_df = self.calc_occupancy_per_minute(ocp_df)
