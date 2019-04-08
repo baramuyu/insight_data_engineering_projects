388c5dfdee38996bbf3545d86c32846d399f4f06
Ubuntu <ubuntu@ip-10-0-0-6.us-west-2.compute.internal>
2019-04-08 03:10:04 +0000

    <feat> show available parking spots on map

--- django/plops/plops_app/static/plops_app/findparking.js
+++ django/plops/plops_app/static/plops_app/findparking.js
@@ -1,6 +1,6 @@
 "use strict";
 
-var parking_markers = [];
+var slot_markers = [];
 
 function findParking(latLng){
     console.log('finding parking.', latLng);
@@ -9,34 +9,33 @@ function findParking(latLng){
     var url = `/app/api?lat=${lat}&lng=${lng}`;
     console.log("url:" + url)
     $.get(url, function(data, status){
-        console.log(data.data[0]['location'])
-        createParkingMarkers(data.data)
+        createParkingMarkers(data.slots)
     }).fail(function() {
         console.log( "coulnd't fetch data." );
   })
 }
 
-function createParkingMarkers(parkings){
-    if (parking_markers) {
-        parking_markers.forEach(function(marker){
+function createParkingMarkers(slots){
+    if (slot_markers) {
+        slot_markers.forEach(function(marker){
             marker.setMap(null);
         })
-        parking_markers = [];
+        slot_markers = [];
     }
-    parkings.forEach(function(parking){
-        parking_markers.push(createMarker(parking))
+    slots.forEach(function(slot){
+        slot_markers.push(createMarker(slot))
     })
 }
 
-function createMarker(parking){
-    var contentString = "<b>Location</b><br>" + parking.station_address;
+function createMarker(slot){
+    var contentString = "<b>Location</b><br>" + slot.station_address;
     var image = '/static/plops_app/image/icon-parking.png'
     var marker = new google.maps.Marker({
-        position: new google.maps.LatLng(parking.location_lat, parking.location_lng),
+        position: new google.maps.LatLng(slot.location_lat, slot.location_lng),
         map: map,
         icon: image,
         animation: google.maps.Animation.DROP,
-        zIndex: Math.round(parking.location_lat*-100000)<<5,
+        zIndex: Math.round(slot.location_lat*-100000)<<5,
     });
     
     google.maps.event.addListener(marker, 'click', function() {
